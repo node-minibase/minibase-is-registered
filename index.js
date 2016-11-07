132e95be85a70bfb2b38f5f4dde1808adc46d3d6
tunnckoCore <mameto_100@mail.bg>
2016-11-07 19:13:57 +0200

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * minibase-is-registered <https://github.com/node-minibase/minibase-is-registered>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * minibase-is-registered <https://github.com/node-minibase/minibase-is-registered>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('mukla')
+
+test(function (done) {
+  done()
+})
