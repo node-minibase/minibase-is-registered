5064ef9fa96a03d3a5fd27f12000d5579e1d70b3
tunnckoCore <mameto_100@mail.bg>
2016-11-07 19:42:54 +0200

    chore(init): implement

--- index.js
+++ index.js
@@ -6,3 +6,20 @@
  */
 
 'use strict'
+
+module.exports = function minibaseIsRegistered (opts) {
+  return function minibaseIsRegistered (self) {
+    var registered = self.registered && typeof self.registered === 'object'
+      ? self.registered
+      : {}
+
+    self.define('registered', registered)
+    self.define('isRegistered', function isRegistered (name) {
+      if (self.registered.hasOwnProperty(name)) {
+        return true
+      }
+      self.registered[name] = true
+      return false
+    })
+  }
+}
--- test.js
+++ test.js
@@ -10,7 +10,45 @@
 'use strict'
 
 var test = require('mukla')
+var isRegistered = require('./index')
+var MiniBase = require('minibase').MiniBase
 
-test(function (done) {
+test('should have `.isRegistered` method', function (done) {
+  var app = new MiniBase()
+  app.use(isRegistered())
+  test.strictEqual(typeof app.isRegistered, 'function')
+  done()
+})
+
+test('should not call plugin twice if it is already registered', function (done) {
+  var app = new MiniBase()
+  var called = 0
+
+  function plugin () {
+    return function foo (self) {
+      if (self.isRegistered('foo')) return
+      called = called + 22
+    }
+  }
+
+  app.use(isRegistered())
+  app.use(plugin())
+  app.use(plugin())
+  test.strictEqual(called, 22)
+  done()
+})
+
+test('should use `app.registered` prop if it is object and already exist', function (done) {
+  var minibase = new MiniBase()
+  var count = 33
+  function fn (self) {
+    if (self.isRegistered('bar')) return
+    count = count + 11
+  }
+  minibase.registered = { bar: true }
+  minibase.use(isRegistered())
+  minibase.use(fn)
+  minibase.use(fn)
+  test.strictEqual(count, 33)
   done()
 })
