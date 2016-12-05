0036cd3ff96701425e4ba188f223700091c94f51
tunnckoCore <mameto_100@mail.bg>
2016-12-05 14:46:48 +0200

    fix(update): update web link and readme

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * minibase-is-registered <https://github.com/node-minibase/minibase-is-registered>
  *
- * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
  * Released under the MIT license.
  */
 
